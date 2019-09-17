10a7872d5cd8e65c0cbe7161fb49a2fc864fe5f2
Chen Bai <baichenno1@gmail.com>
2019-09-17 15:38:57 +0800

    microservice provider user my metadata

---
+++ microservice-provider-user-my-metadata/src/main/java/com/baichen/cloud/microserviceproviderusermymetadata/MicroserviceProviderUserMyMetadataApplication.java
@@ -0,0 +1,13 @@
+package com.baichen.cloud.microserviceproviderusermymetadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProviderUserMyMetadataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceProviderUserMyMetadataApplication.class, args);
+    }
+
+}
