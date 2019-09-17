0e61e0b42b535e3df7c1c91c7af26eded0abd1d7
Chen Bai <baichenno1@gmail.com>
2019-09-17 18:04:59 +0800

    update file

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/baichen/cloud/microserviceconsumermovieribbon/MicroserviceConsumerMovieRibbonApplication.java
@@ -0,0 +1,23 @@
+package com.baichen.cloud.microserviceconsumermovieribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//@EnableDiscoveryClient(autoRegister = false)
+public class MicroserviceConsumerMovieRibbonApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate testTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieRibbonApplication.class, args);
+    }
+
+}
