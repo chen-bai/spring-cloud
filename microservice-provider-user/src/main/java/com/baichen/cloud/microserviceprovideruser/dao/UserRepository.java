9c818f1b799394791fde39504d065859737f874d
Chen Bai <baichenno1@gmail.com>
2019-09-17 15:37:39 +0800

    remove files

--- microservice-provider-user/src/main/java/com/baichen/cloud/microserviceprovideruser/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baichen.cloud.microserviceprovideruser.dao;
-
-import com.baichen.cloud.microserviceprovideruser.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
