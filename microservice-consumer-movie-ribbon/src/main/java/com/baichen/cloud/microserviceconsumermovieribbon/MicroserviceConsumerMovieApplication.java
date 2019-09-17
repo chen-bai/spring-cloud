d82d686e48b0037cf568d5c2801cfe575f5e269e
Chen Bai <baichenno1@gmail.com>
2019-09-17 17:27:53 +0800

    integrated ribbon for loadbalancing

--- microservice-consumer-movie-ribbon/src/main/java/com/baichen/cloud/microserviceconsumermovieribbon/MicroserviceConsumerMovieApplication.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/baichen/cloud/microserviceconsumermovieribbon/MicroserviceConsumerMovieApplication.java
@@ -2,6 +2,7 @@ package com.baichen.cloud.microserviceconsumermovieribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class MicroserviceConsumerMovieApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate testTemplate() {
         return new RestTemplate();
     }
