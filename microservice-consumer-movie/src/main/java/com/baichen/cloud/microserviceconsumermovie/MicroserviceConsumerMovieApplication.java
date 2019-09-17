641b1a221bdaae65772cd606ff250a108ea93d6c
Chen Bai <baichenno1@gmail.com>
2019-09-17 11:31:59 +0800

    microservice consumer movie

---
+++ microservice-consumer-movie/src/main/java/com/baichen/cloud/microserviceconsumermovie/MicroserviceConsumerMovieApplication.java
@@ -0,0 +1,20 @@
+package com.baichen.cloud.microserviceconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MicroserviceConsumerMovieApplication {
+
+    @Bean
+    public RestTemplate testTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieApplication.class, args);
+    }
+
+}
