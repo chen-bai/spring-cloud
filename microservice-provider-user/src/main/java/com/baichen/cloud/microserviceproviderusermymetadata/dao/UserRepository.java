c06c371c65f98f4d0806c46622b0f3df7ed0202b
Chen Bai <baichenno1@gmail.com>
2019-09-17 17:36:07 +0800

    restore provider package name

--- microservice-provider-user/src/main/java/com/baichen/cloud/microserviceproviderusermymetadata/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baichen.cloud.microserviceproviderusermymetadata.dao;
-
-import com.baichen.cloud.microserviceproviderusermymetadata.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
