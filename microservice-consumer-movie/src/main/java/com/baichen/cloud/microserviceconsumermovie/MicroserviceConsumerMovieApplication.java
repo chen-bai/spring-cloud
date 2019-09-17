5ac01cf1218e1ec10a102395064dbf405a456f0d
Chen Bai <baichenno1@gmail.com>
2019-09-17 11:43:34 +0800

    two ways to disable register service

--- microservice-consumer-movie/src/main/java/com/baichen/cloud/microserviceconsumermovie/MicroserviceConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/com/baichen/cloud/microserviceconsumermovie/MicroserviceConsumerMovieApplication.java
@@ -2,10 +2,12 @@ package com.baichen.cloud.microserviceconsumermovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+//@EnableDiscoveryClient(autoRegister = false)
 public class MicroserviceConsumerMovieApplication {
 
     @Bean
