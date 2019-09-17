75e88c37fe4b17dca8e67959662169f593397199
Chen Bai <baichenno1@gmail.com>
2019-09-17 17:10:39 +0800

    microservice consumer movie ribbon

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/baichen/cloud/microserviceconsumermovieribbon/MicroserviceConsumerMovieApplication.java
@@ -0,0 +1,21 @@
+package com.baichen.cloud.microserviceconsumermovieribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//@EnableDiscoveryClient(autoRegister = false)
+public class MicroserviceConsumerMovieApplication {
+
+    @Bean
+    public RestTemplate testTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieApplication.class, args);
+    }
+
+}
