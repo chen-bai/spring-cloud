aa4c229e7cfaf4fd1d8ae66f64d2783a03452c52
Chen Bai <baichenno1@gmail.com>
2019-09-18 10:57:17 +0800

    microservice consumer movie ribbon without eureka

---
+++ microservice-consumer-movie-ribbon-without-eureka/src/main/java/com/baichen/cloud/microserviceconsumermovieribbonwithouteureka/MicroserviceConsumerMovieRibbonWithoutEurekaApplication.java
@@ -0,0 +1,23 @@
+package com.baichen.cloud.microserviceconsumermovieribbonwithouteureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//@EnableDiscoveryClient(autoRegister = false)
+public class MicroserviceConsumerMovieRibbonWithoutEurekaApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate testTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieRibbonWithoutEurekaApplication.class, args);
+    }
+
+}
