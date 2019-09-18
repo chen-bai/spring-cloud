bd799eba4d2140cc8de44b16ff8f2c9e792b2127
Chen Bai <baichenno1@gmail.com>
2019-09-18 15:21:44 +0800

    global feign default config

--- microservice-consumer-movie-feign-customizing/src/main/java/com/baichen/cloud/microserviceconsumermoviefeigncustomizing/MicroserviceConsumerMovieFeignCustomizingApplication.java
+++ microservice-consumer-movie-feign-customizing/src/main/java/com/baichen/cloud/microserviceconsumermoviefeigncustomizing/MicroserviceConsumerMovieFeignCustomizingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+//@EnableFeignClients(defaultConfiguration = DefaultRibbonConfig.class)
 @EnableDiscoveryClient
 public class MicroserviceConsumerMovieFeignCustomizingApplication {
 
