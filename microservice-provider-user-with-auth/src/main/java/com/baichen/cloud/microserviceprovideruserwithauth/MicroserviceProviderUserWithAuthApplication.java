66629c5ba31e5b306714f820a13de9bf825565fa
Chen Bai <baichenno1@gmail.com>
2019-09-18 18:59:13 +0800

    microservice provider user with auth

---
+++ microservice-provider-user-with-auth/src/main/java/com/baichen/cloud/microserviceprovideruserwithauth/MicroserviceProviderUserWithAuthApplication.java
@@ -0,0 +1,13 @@
+package com.baichen.cloud.microserviceprovideruserwithauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProviderUserWithAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceProviderUserWithAuthApplication.class, args);
+    }
+
+}
---
+++ microservice-provider-user-with-auth/src/main/java/com/baichen/cloud/microserviceprovideruserwithauth/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.baichen.cloud.microserviceprovideruserwithauth.controller;
+
+import com.baichen.cloud.microserviceprovideruserwithauth.repository.UserRepository;
+import com.baichen.cloud.microserviceprovideruserwithauth.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            UserDetails user = (UserDetails) principal;
+            Collection<? extends GrantedAuthority> collection = user.getAuthorities();
+            for (GrantedAuthority c : collection) {
+                UserController.LOGGER.info("当前用户是{}，角色是{}", user.getUsername(), c.getAuthority());
+            }
+        } else {
+            UserController.LOGGER.error("没有该用户");
+        }
+        Optional<User> optionalUser = this.userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+        } else {
+            return null;
+        }
+    }
+}
