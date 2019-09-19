61313574a15fc7206bdbbe8e6a3814b73c7afb60
Chen Bai <baichenno1@gmail.com>
2019-09-19 10:08:39 +0800

    microservice consumer movie feign manual

---
+++ microservice-consumer-movie-feign-manual/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignmanual/MicroserviceConsumerMovieFeignManualApplication.java
@@ -0,0 +1,17 @@
+package com.baichen.cloud.microserviceconsumermoviefeignmanual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class MicroserviceConsumerMovieFeignManualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieFeignManualApplication.class, args);
+    }
+
+}
---
+++ microservice-consumer-movie-feign-manual/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignmanual/user/controller/MovieController.java
@@ -0,0 +1,19 @@
+package com.baichen.cloud.microserviceconsumermoviefeignmanual.user.controller;
+
+import com.baichen.cloud.microserviceconsumermoviefeignmanual.user.entity.User;
+import com.baichen.cloud.microserviceconsumermoviefeignmanual.user.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userFeignClient.findById(id);
+    }
+}
