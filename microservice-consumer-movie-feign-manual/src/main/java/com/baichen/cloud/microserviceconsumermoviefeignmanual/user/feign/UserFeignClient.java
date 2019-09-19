09c4c3a26287cfbb64b55ceadea83419031c5511
Chen Bai <baichenno1@gmail.com>
2019-09-19 10:39:06 +0800

    feign manual config

--- microservice-consumer-movie-feign-manual/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignmanual/user/feign/UserFeignClient.java
+++ microservice-consumer-movie-feign-manual/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignmanual/user/feign/UserFeignClient.java
@@ -1,13 +1,11 @@
 package com.baichen.cloud.microserviceconsumermoviefeignmanual.user.feign;
 
 import com.baichen.cloud.microserviceconsumermoviefeignmanual.user.entity.User;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name ="microservice-provider-user")
 public interface UserFeignClient {
-    @RequestMapping(value="/{id}", method= RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     User findById(@PathVariable("id") Long id);
 }
