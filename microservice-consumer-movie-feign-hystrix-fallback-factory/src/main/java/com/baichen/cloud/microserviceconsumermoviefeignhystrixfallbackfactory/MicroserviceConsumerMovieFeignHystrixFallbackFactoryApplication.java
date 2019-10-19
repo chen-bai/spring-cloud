e24da44f29e1be5825ba0e9f3ca3d0c9a203000f
Chen Bai <baichenno1@gmail.com>
2019-10-19 11:40:40 +0800

    microservice consumer movie feign hystrix fallback factory

---
+++ microservice-consumer-movie-feign-hystrix-fallback-factory/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignhystrixfallbackfactory/MicroserviceConsumerMovieFeignHystrixFallbackFactoryApplication.java
@@ -0,0 +1,17 @@
+package com.baichen.cloud.microserviceconsumermoviefeignhystrixfallbackfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class MicroserviceConsumerMovieFeignHystrixFallbackFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieFeignHystrixFallbackFactoryApplication.class, args);
+    }
+
+}
