cd8fcf0e66bf997b6df8d57f4df808672835a9ed
Chen Bai <baichenno1@gmail.com>
2019-10-19 15:27:30 +0800

    add hystrix stream to feign client

--- microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignhystrixfallbackstream/MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication.java
+++ microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignhystrixfallbackstream/MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication.java
@@ -1,17 +1,28 @@
 package com.baichen.cloud.microserviceconsumermoviefeignhystrixfallbackstream;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableHystrix
 public class MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/actuator/hystrix.stream");
+        return registration;
+    }
 }
