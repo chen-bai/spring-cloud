b05d7d7ab02d0360a6bf0418ededd3c9010a9c33
Chen Bai <baichenno1@gmail.com>
2019-10-19 10:41:35 +0800

    microservice consumer movie ribbon hystrix

---
+++ microservice-consumer-movie-ribbon-hystrix/src/main/java/com/baichen/cloud/microserviceconsumermovieribbonhystrix/MicroserviceConsumerMovieRibbonHystrixApplication.java
@@ -0,0 +1,25 @@
+package com.baichen.cloud.microserviceconsumermovieribbonhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//@EnableDiscoveryClient(autoRegister = false)
+@EnableHystrix
+public class MicroserviceConsumerMovieRibbonHystrixApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate testTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieRibbonHystrixApplication.class, args);
+    }
+
+}
