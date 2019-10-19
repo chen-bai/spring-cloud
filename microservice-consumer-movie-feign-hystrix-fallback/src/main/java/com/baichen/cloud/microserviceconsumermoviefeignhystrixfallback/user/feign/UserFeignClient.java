2b30ad8a84f2890719d6ceeff8ce9d5a40155177
Chen Bai <baichenno1@gmail.com>
2019-10-19 11:17:40 +0800

    add feign clien fallback class

--- microservice-consumer-movie-feign-hystrix-fallback/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignhystrixfallback/user/feign/UserFeignClient.java
+++ microservice-consumer-movie-feign-hystrix-fallback/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignhystrixfallback/user/feign/UserFeignClient.java
@@ -2,12 +2,34 @@ package com.baichen.cloud.microserviceconsumermoviefeignhystrixfallback.user.fei
 
 import com.baichen.cloud.microserviceconsumermoviefeignhystrixfallback.user.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name ="microservice-provider-user")
+/**
+ * Feign的fallback测试
+ * 使用@FeignClient的fallback属性指定回退类
+ * @author baichen
+ */
+@FeignClient(name ="microservice-provider-user", fallback=FeignClientFallback.class)
 public interface UserFeignClient {
     @RequestMapping(value="/{id}", method= RequestMethod.GET)
     User findById(@PathVariable("id") Long id);
+}
+
+/**
+ * 回退类FeignClientFallback需实现Feign Client接口
+ * FeignClientFallback也可以是public class，没有区别
+ * @author baichen
+ */
+@Component
+class FeignClientFallback implements UserFeignClient {
+    @Override
+    public User findById(Long id) {
+        User user = new User();
+        user.setId(-1L);
+        user.setName("default user");
+        return user;
+    }
 }
