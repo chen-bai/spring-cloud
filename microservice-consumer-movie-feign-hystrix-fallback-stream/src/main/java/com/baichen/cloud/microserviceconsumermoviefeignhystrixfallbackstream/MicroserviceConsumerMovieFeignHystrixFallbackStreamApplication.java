768e960be954be2ca6c982bbf277855db70e4aa5
Chen Bai <baichenno1@gmail.com>
2019-10-19 15:33:33 +0800

    add hystrix dashboard

--- microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignhystrixfallbackstream/MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication.java
+++ microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/baichen/cloud/microserviceconsumermoviefeignhystrixfallbackstream/MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication {
 
     public static void main(String[] args) {
