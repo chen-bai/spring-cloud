15cbd481c5e491a3964601385a986fb3f99d2631
Chen Bai <baichenno1@gmail.com>
2019-10-23 16:21:05 +0800

    microservice gateway zuul

---
+++ microservice-gateway-zuul/src/test/java/com/baichen/cloud/microservicegatewayzuul/MicroserviceGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baichen.cloud.microservicegatewayzuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
