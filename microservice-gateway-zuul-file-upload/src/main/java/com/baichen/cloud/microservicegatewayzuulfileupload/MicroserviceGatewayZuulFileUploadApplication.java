525d278d6321538cf8389bf4096c9d1526b9fd65
Chen Bai <baichenno1@gmail.com>
2019-10-23 18:24:25 +0800

    microservice gateway zuul file upload

---
+++ microservice-gateway-zuul-file-upload/src/main/java/com/baichen/cloud/microservicegatewayzuulfileupload/MicroserviceGatewayZuulFileUploadApplication.java
@@ -0,0 +1,24 @@
+package com.baichen.cloud.microservicegatewayzuulfileupload;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class MicroserviceGatewayZuulFileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceGatewayZuulFileUploadApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/actuator/hystrix.stream");
+        return registration;
+    }
+}
