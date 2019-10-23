87743a6120694bdc2c31a4d8d24bf1d6613723b6
Chen Bai <baichenno1@gmail.com>
2019-10-23 16:31:21 +0800

    add hystrix.stream page

--- microservice-gateway-zuul/src/main/java/com/baichen/cloud/microservicegatewayzuul/MicroserviceGatewayZuulApplication.java
+++ microservice-gateway-zuul/src/main/java/com/baichen/cloud/microservicegatewayzuul/MicroserviceGatewayZuulApplication.java
@@ -1,8 +1,11 @@
 package com.baichen.cloud.microservicegatewayzuul;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +15,10 @@ public class MicroserviceGatewayZuulApplication {
         SpringApplication.run(MicroserviceGatewayZuulApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/actuator/hystrix.stream");
+        return registration;
+    }
 }
