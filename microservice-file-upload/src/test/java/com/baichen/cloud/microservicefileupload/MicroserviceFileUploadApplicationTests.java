4f2f3570bd6f77b80ad671b86f983d6ce36b1b14
Chen Bai <baichenno1@gmail.com>
2019-10-23 18:10:45 +0800

    microservice file upload

---
+++ microservice-file-upload/src/test/java/com/baichen/cloud/microservicefileupload/MicroserviceFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baichen.cloud.microservicefileupload;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
