a7c2dbef549632bdee87399c2218323757f2b5a5
Chen Bai <baichenno1@gmail.com>
2019-10-23 16:31:48 +0800

    microservice hystrix dashboard

---
+++ microservice-hystrix-dashboard/src/test/java/com/baichen/cloud/microservicehystrixdashboard/MicroserviceHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baichen.cloud.microservicehystrixdashboard;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
