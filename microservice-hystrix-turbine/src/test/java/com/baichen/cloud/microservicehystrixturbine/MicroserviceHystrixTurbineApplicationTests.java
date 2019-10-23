ee05086aff22ef66e2256d1760f9fcb8c87bc18e
Chen Bai <baichenno1@gmail.com>
2019-10-23 15:10:28 +0800

    microservice hystrix turbine

---
+++ microservice-hystrix-turbine/src/test/java/com/baichen/cloud/microservicehystrixturbine/MicroserviceHystrixTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baichen.cloud.microservicehystrixturbine;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceHystrixTurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
